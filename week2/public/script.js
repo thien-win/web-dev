e023de61db722a1afe6ca1453489b29028f864bd
LT <42130987+pumpjackrider380@users.noreply.github.com>
2020-06-26 17:49:21 -0500

    Update script.js

--- week2/public/script.js
+++ week2/public/script.js
@@ -1,12 +1,3 @@
-var express = require('express');
-var app = express();
-var path = require("path")
+let  submit = document.querySelector('.login';
+let post = document.querySelector('.post';
 
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/index.html'));
-});
-
-app.get(/user/', (req, res) ==>
-app.listen(3000, function(){
-    console.log('Listening to port: '+ 3000 );
-});
