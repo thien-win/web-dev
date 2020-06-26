2163926d4c9abe012b8a70521a678bc72ca4e469
LT <42130987+pumpjackrider380@users.noreply.github.com>
2020-06-26 17:47:47 -0500

    Update app.js

--- week2/app.js
+++ week2/app.js
@@ -2,13 +2,18 @@ var express = require('express');
 var app = express();
 var path = require("path")
 
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/public/user.html'));
+app.use(express.static('public'));
+app.use(express.json());
+
+app.post('/user', (req, res) => {
+    console.log(req.body);
+    res.send('Created!');
 });
 
-app.get('/user/', (req, res) => {
-    res.send({id:2, login: 'Nancy', age:20});
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
 });
+
 
 app.listen(3000, function(){
     console.log('Listening to port: '+ 3000 );
